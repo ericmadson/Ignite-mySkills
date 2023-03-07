9296ed739b061679a008b95b33ca0221c1813da0
Eric Madson Almeida <ericmadson67@hotmail.com>
2023-03-06 23:10:57 -0300

    statusBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   Platform,
-  ScrollView,
   FlatList,
 } from 'react-native';
 import {Button} from '../components/Button';
@@ -14,15 +13,31 @@ import {SkillCard} from '../components/SkillCard';
 export default function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [greeting, setGreeting] = useState('');
 
   /* Use handle to name functions when the function is triggered by user's interaction */
   function handleAddNewSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHours = new Date().getHours();
+
+    if (currentHours < 12) {
+      setGreeting('Good Morning');
+    } else if(currentHours >=12 && currentHours < 18) {
+      setGreeting('Good Afternoon');
+    } else {
+      setGreeting('Good Night');
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
+      
       <Text style={styles.title}>Welcome, Eric</Text>
+
+      <Text style={styles.greetings}>{greeting}</Text>
 
       <TextInput
         style={styles.input}
@@ -65,5 +80,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#fff',
   },
 });
